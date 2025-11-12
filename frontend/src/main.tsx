fd8d34e79f9e75b682df7421c4168fb84f7cd315
aaron031291 <shipton1234@gmail.com>
2025-11-12 19:01:49 +0000

    feat: Enable full AppSimple tab UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.minimal.tsx'
+import App from './AppSimple.tsx'
 import { ErrorBoundary } from './components/ErrorBoundary'
 
 createRoot(document.getElementById('root')!).render(
